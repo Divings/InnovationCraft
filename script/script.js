aad296b6a4465ef0a2f5e93936b42252de4002db
Divings <vaildec5@outlook.com>
2023-09-25 14:11:56 +0900

    First Commit

---
+++ script/script.js
@@ -0,0 +1,24 @@
+function index(){
+location.href = "index.html";
+}
+function button(){
+location.href = "download.htm";
+}
+function button2(){
+location.href = "a/creators.php";
+}
+function button3(){
+location.href = "a/info.html";
+}
+function button4(){
+location.href = "a/creators/index.php";
+}
+function contact(){
+location.href = "https://form1ssl.fc2.com/post/?id=cc6c6c97a3ab6b33";
+}
+function button5(){
+location.href = "https://objectstorage.ap-tokyo-1.oraclecloud.com/p/2XBgWACcTZI5K9IGm5RhFi7PtT4vFKwSVPLZEv6ZdNBKpp8KRTGid-2OhVUjzX9_/n/nrwzxiqvwvkf/b/Notepad/o/Dec/Dec.zip";
+}
+function button6(){
+location.href = "https://github.com/Divings/Dec";
+}
