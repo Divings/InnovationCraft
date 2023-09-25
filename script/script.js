4d0ba32ab074b933bb1f713a212b94442b102bc1
Divings <vaildec5@outlook.com>
2023-09-25 14:31:41 +0900

    index.htmlの変更

--- script/script.js
+++ script/script.js
@@ -19,6 +19,9 @@ location.href = "https://form1ssl.fc2.com/post/?id=cc6c6c97a3ab6b33";
 function button5(){
 location.href = "https://objectstorage.ap-tokyo-1.oraclecloud.com/p/2XBgWACcTZI5K9IGm5RhFi7PtT4vFKwSVPLZEv6ZdNBKpp8KRTGid-2OhVUjzX9_/n/nrwzxiqvwvkf/b/Notepad/o/Dec/Dec.zip";
 }
+function installer(){
+    location.href = "https://objectstorage.ap-tokyo-1.oraclecloud.com/p/2XBgWACcTZI5K9IGm5RhFi7PtT4vFKwSVPLZEv6ZdNBKpp8KRTGid-2OhVUjzX9_/n/nrwzxiqvwvkf/b/Notepad/o/Dec/installer/Dec.zip";
+    }
 function button6(){
 location.href = "https://github.com/Divings/Dec";
 }
